5464925f56e458b6a7554772b2f177d0950a87d7
AndrewReis <andrew-reiis@outlook.com>
2025-07-31 08:19:40 -0300

    init

---
+++ index.mjs
@@ -0,0 +1,8 @@
+export const handler = async (event) => {
+  console.log('EVENT: ', JSON.stringify(event, null, 2));
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Github Actions!'),
+  };
+  return response;
+};
